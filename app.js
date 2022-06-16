a5d44a1ae505a607b74835db203d374438846e61
fquimbay05 <fquimbay@summa-sci.com>
2022-06-16 18:14:59 -0500

    Iconos redes sociales HTML

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express"); //Requerimos el modulo Express
+const app = express(); //Ejecutamos la función, creamos una variable y almacenamos express.
+const path = require('path')
+//const rutaAbsoluta='./views/';
+app.use(express.static('public'))
+
+
+app.listen(3040, () => {console.log("Servidor ejecutando")});
+
+app.get("/", (req, res) => {
+    let index= path.resolve('./views/index.html');
+    res.sendFile(index);
+  })
