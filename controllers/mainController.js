0df5c9ec7ebc740e62311bbdd7851b674d006a45
fquimbay05 <fquimbay@summa-sci.com>
2022-08-03 13:29:46 -0500

    depuracion comentarios

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,44 +1,84 @@
 const listaRecetas = [
-    {
-        id:1,
-        titulo: 'Mojito',
-        descripcion: 'Este cóctel tan famoso a nivel mundial hace patria de Cuba, el país que lo vio nacer',
-        html: 'mojito.html',
-        ingredientes: ['5 cl de tequila', '2,5 cl de triple seco que es licor de naranja. Ejemplos de licores triple seco son Cointreau o Grand Marnier.', '2,5 cl de zumo de lima recién exprimido y un trocito de lima para mojar el borde de las copas.', 'Sal', 'Hielos'],        
-        instrucciones: ['Coloca una rama de menta en cada vaso, agrega un caballito de ron, una cucharada de azúcar y un par de rodajas de limón.','Aplasta con un pilón y después agrega el agua mineral y hielo'],
-        img: 'mojito.jpg'
-    },
-    {
-        id:2,
-        titulo: 'Margarita',
-        descripcion: 'Es un cóctel originario de México',   
-        ingredientes: ['5 cl de tequila', '2,5 cl de triple seco que es licor de naranja. Ejemplos de licores triple seco son Cointreau o Grand Marnier.', '2,5 cl de zumo de lima recién exprimido y un trocito de lima para mojar el borde de las copas.', 'Sal', 'Hielos'],   
-        instrucciones: ['Primero decoraremos el borde de sal. Cogemos un vaso ancho y ovalado (el clásico de margarita) lo mojamos en un poco de sirope de azúcar presionando y a continuación, lo pasamos por un plato con sal, colocamos la lima y reservamos', 'Exprimimos las limas para extraer su zumo', 'Vertemos en la coctelera el zumo de lima recién exprimido, el tequila, y el triple seco, en este orden','Adicionamos cuatro o cinco cubos de hielo y agitamos bien durante unos 15 segundos', 'Servimos el cóctel con ayuda del filtro de la propia coctelera'],                  
-        html: 'margarita.html',
-        img: 'margarita.jpg'
-    },
-    {
-        id:3,
-        titulo: 'Martini',
-        descripcion: 'Martini, Dry Martini o Martini seco, un cóctel exquisito y sencillo',
-        ingredientes: ['60 mL ginebra', '30 mL vermut seco'],     
-        instrucciones: ['En un vaso mezclador agregarla ginebra, vermut seco, e hielo.','Revolver ingredientes por 20-25 segundos.','Usando un colador Julep colar el cóctel y servir en la copa cupé.','Adornar con una aceituna o un toque de limón'],                      
-        html: 'martini.html',
-        img: 'martini.jpg'
-    },
-    {
-        id:4,
-        titulo: 'Daiquiri',
-        descripcion: 'El cóctel cubano por excelencia',
-        ingredientes: ['50 ml de ron blanco', '25 ml de zumo de lima', '1/2 cucharadita de postre de azúcar glas o jarabe de azúcar', '5 Cubos de Hielo', '1 limón en rodajas'],    
-        instrucciones: ['Exprime la lima para extraer su zumo y verter en la coctelera', 'Adiciona el azúcar glas o el jarabe de azúcar y el ron blanco', 'Añadimos los cubos de hielo', 'Tapamos y agitamos la coctelera durante 15 segundos', 'Vierte la bebida en la copa y decórala con una rodaja de lima'],              
-        html: 'daiquiri.html',
-        img: 'daiquiri.jpg'
-    }
-]
+  {
+    id: 1,
+    titulo: "Mojito",
+    descripcion:
+      "Este cóctel tan famoso a nivel mundial hace patria de Cuba, el país que lo vio nacer",
+    html: "mojito.html",
+    ingredientes: [
+      "5 cl de tequila",
+      "2,5 cl de triple seco que es licor de naranja. Ejemplos de licores triple seco son Cointreau o Grand Marnier.",
+      "2,5 cl de zumo de lima recién exprimido y un trocito de lima para mojar el borde de las copas.",
+      "Sal",
+      "Hielos",
+    ],
+    instrucciones: [
+      "Coloca una rama de menta en cada vaso, agrega un caballito de ron, una cucharada de azúcar y un par de rodajas de limón.",
+      "Aplasta con un pilón y después agrega el agua mineral y hielo",
+    ],
+    img: "mojito.jpg",
+  },
+  {
+    id: 2,
+    titulo: "Margarita",
+    descripcion: "Es un cóctel originario de México",
+    ingredientes: [
+      "5 cl de tequila",
+      "2,5 cl de triple seco que es licor de naranja. Ejemplos de licores triple seco son Cointreau o Grand Marnier.",
+      "2,5 cl de zumo de lima recién exprimido y un trocito de lima para mojar el borde de las copas.",
+      "Sal",
+      "Hielos",
+    ],
+    instrucciones: [
+      "Primero decoraremos el borde de sal. Cogemos un vaso ancho y ovalado (el clásico de margarita) lo mojamos en un poco de sirope de azúcar presionando y a continuación, lo pasamos por un plato con sal, colocamos la lima y reservamos",
+      "Exprimimos las limas para extraer su zumo",
+      "Vertemos en la coctelera el zumo de lima recién exprimido, el tequila, y el triple seco, en este orden",
+      "Adicionamos cuatro o cinco cubos de hielo y agitamos bien durante unos 15 segundos",
+      "Servimos el cóctel con ayuda del filtro de la propia coctelera",
+    ],
+    html: "margarita.html",
+    img: "margarita.jpg",
+  },
+  {
+    id: 3,
+    titulo: "Martini",
+    descripcion:
+      "Martini, Dry Martini o Martini seco, un cóctel exquisito y sencillo",
+    ingredientes: ["60 mL ginebra", "30 mL vermut seco"],
+    instrucciones: [
+      "En un vaso mezclador agregarla ginebra, vermut seco, e hielo.",
+      "Revolver ingredientes por 20-25 segundos.",
+      "Usando un colador Julep colar el cóctel y servir en la copa cupé.",
+      "Adornar con una aceituna o un toque de limón",
+    ],
+    html: "martini.html",
+    img: "martini.jpg",
+  },
+  {
+    id: 4,
+    titulo: "Daiquiri",
+    descripcion: "El cóctel cubano por excelencia",
+    ingredientes: [
+      "50 ml de ron blanco",
+      "25 ml de zumo de lima",
+      "1/2 cucharadita de postre de azúcar glas o jarabe de azúcar",
+      "5 Cubos de Hielo",
+      "1 limón en rodajas",
+    ],
+    instrucciones: [
+      "Exprime la lima para extraer su zumo y verter en la coctelera",
+      "Adiciona el azúcar glas o el jarabe de azúcar y el ron blanco",
+      "Añadimos los cubos de hielo",
+      "Tapamos y agitamos la coctelera durante 15 segundos",
+      "Vierte la bebida en la copa y decórala con una rodaja de lima",
+    ],
+    html: "daiquiri.html",
+    img: "daiquiri.jpg",
+  },
+];
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const mainController = {
   index: (req, res) => {
@@ -48,39 +88,35 @@ const mainController = {
     // let plato = listaRecetas.find(plato => plato.id == req.params.menuId);
     // res.render("receta", { receta: receta });
     // res.send('Listado de Recetas pruebas');
-    res.render("receta",{listaRecetas});
+    res.render("receta", { listaRecetas });
   },
   listar: (req, res) => {
     res.send("Receta " + req.params.idReceta);
-  },  
+  },
   edit: (req, res) => {
     //res.send("Receta  " + req.params.idReceta);
-     let id = req.params.idReceta;
-     let receta = listaRecetas.find(receta => receta.id == id);
-     let rederizar = receta.html
-     console.log(receta);
-     //res.render(rederizar);
-     res.render("cocteles", { receta: receta });
+    let id = req.params.idReceta;
+    let receta = listaRecetas.find((receta) => receta.id == id);
+    let rederizar = receta.html;
+    console.log(receta);
+    //res.render(rederizar);
+    res.render("cocteles", { receta: receta });
     // res.send('Listado de Recetas');
     //res.render("receta");
   },
   editar: (req, res) => {
     res.send("Receta: " + req.params.idReceta);
-  }, 
+  },
   servicio: (req, res) => {
-    // res.send("Servicios: " );
-    res.render("receta",{listaRecetas});
-  },  
+    res.render("receta", { listaRecetas });
+  },
   login: (req, res) => {
-    // res.send("Servicios: " );
     res.render("login");
-  },  
+  },
   registro: (req, res) => {
-    // res.send("Servicios: " );
     res.render("registro");
   },
   compras: (req, res) => {
-    // res.send("Servicios: " );
     res.render("compras");
   },
 };
